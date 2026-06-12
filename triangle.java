8f8ccafaab0fd803e748393c9147a7186a9f9fc2
Malo-lab07 <assensomarfolouis@gmail.com>
2026-06-12 21:37:56 +0000

    Add files via upload

---
+++ triangle.java
@@ -0,0 +1,12 @@
+public class triangle {
+
+    static void main() {
+        System.out.println("   T");
+        System.out.println("  TTT ");
+        System.out.println(" TTTTT");
+        System.out.println("TTTTTTT");
+        System.out.println("TTTTTTT");
+
+
+    }
+}
